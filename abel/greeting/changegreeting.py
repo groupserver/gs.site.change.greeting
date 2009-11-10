7d65f0305800f1df8b4f03c00f13a47a801d9c3a
Michael JasonSmith <mpj17@onlinegroups.net>
2009-11-10 13:20:04 +1100

    Tweeks

--- abel/greeting/changegreeting.py
+++ abel/greeting/changegreeting.py
@@ -14,6 +14,30 @@ class ChangeGreeting(PageForm):
     def __init__(self, context, request):
         PageForm.__init__(self, context, request)
         self.siteInfo = createObject('groupserver.SiteInfo', context)
+        self.__groupsInfo = None
+        self.__userInfo = None
+        
+    @property
+    def userInfo(self):
+        if self.__userInfo == None:
+            self.__userInfo = createObject('groupserver.LoggedInUser', 
+              self.context)
+        return self.__userInfo
+    
+    @property
+    def groupsInfo(self):
+        if  self.__groupsInfo == None:
+            self.__groupsInfo = createObject('groupserver.GroupsInfo', 
+                self.context)
+        return self.__groupsInfo
+        
+    def groupMembership(self):
+        groups = self.groupsInfo.get_member_groups_for_user(
+            self.userInfo.user, self.userInfo.user)
+        retval = [createObject('groupserver.GroupInfo', g) for g in groups]
+        retval.sort(key=lambda group: group.name.lower())
+        assert type(retval) == list
+        return retval
 
     @form.action(label=u'Change', failure='handle_change_action_failure')
     def handle_set(self, action, data):
